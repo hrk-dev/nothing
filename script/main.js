ed9ddea4ca470082e57c76d7cd10a18e7e3b3a75
blacktunes <blacktunes@foxmail.com>
2021-06-05 14:54:44 +0800

    feat: 修改加/解密为单条信息

--- script/main.js
+++ script/main.js
@@ -5,105 +5,119 @@ var pusher = new Pusher('d392f65ca7b23a30e14c', {
 })
 
 var channel = pusher.subscribe('hrk-blog')
-channel.bind('set-normal', () => {
-  diary.setNormal()
+channel.bind('set-normal', (data) => {
+  diary.setNormal(data.id)
 })
-channel.bind('set-abnormal', () => {
-  diary.setAbnormal()
+channel.bind('set-abnormal', (data) => {
+  diary.setAbnormal(data.id)
 })
 
 var diary = new Vue({
   el: '#diary',
   data: () => ({
-    messages: [],
-    wait: false,
+    loading: true,
     ready: false,
-    normal: false,
-    encryption: true,
-    blur: 0.5,
     msgList: [
       {
+        normal: false,
+        blur: 0.5,
+        wait: false,
         date: '3.3',
         msg: 'JUU1JTlCJUEwJUU0JUI4JUJBJUU1JUFFJTlFJUU1JTlDJUE4JUU2JTk4JUFGJUU0JUI4JThEJUU3JTlGJUE1JUU5JTgxJTkzJUU1JUJBJTk0JUU4JUFGJUE1JUU1JTg2JTk5JUU0JUJBJTlCJUU0JUJCJTgwJUU0JUI5JTg4JUU2JTg5JTgwJUU0JUJCJUE1JUU1JUIwJUIxJUU5JTk4JUJGJUU1JUI3JUI0JUU5JTk4JUJGJUU1JUI3JUI0JUU5JTk4JUJGJUU1JUI3JUI0JUU5JTk4JUJGJUU1JUI3JUI0JUU5JTk4JUJGJUU1JUI3JUI0JUU5JTk4JUJGJUU1JUI3JUI0JUU5JTk4JUJGJUU1JUI3JUI0JUU5JTk4JUJGJUU1JUI3JUI0JUU5JTk4JUJGJUU1JUI3JUI0JUU5JTk4JUJGJUU1JUI3JUI0JUU5JTk4JUJGJUU1JUI3JUI0JUU5JTk4JUJGJUU1JUI3JUI0JUU5JTk4JUJGJUU1JUI3JUI0JUU5JTk4JUJGJUU1JUI3JUI0JUU5JTk4JUJGJUU1JUI3JUI0JUU5JTk4JUJGJUU1JUI3JUI0JUU5JTk4JUJGJUU1JUI3JUI0JUU5JTk4JUJGJUU1JUI3JUI0',
       },
       {
+        normal: false,
+        blur: 0.5,
+        wait: false,
         date: '3.4',
         msg: 'QmVjYXVzZSUyMEklMjByZWFsbHklMjBkb24ndCUyMGtub3clMjB3aGF0JTIwdG8lMjB3cml0ZSUyQyUyMHNvJTIwSSUyMGp1c3QlMjB3YW50JTIwdG8lMjB3cml0ZSUyMGFiYSUyMGFiYSUyMGFiYSUyMGFiYSUyMGFiYSUyMGFiYSUyMGFiYSUyMGFiYSUyMGFiYSUyMGFiYSUyMGFiYSUyMGFiYSUyMGFiYSUyMGFiYSUyMGFiYSUyMGFiYSUyMGFiYSUyMGFiYSUyMGFiYSUyMGFiYSUyMGFiYSUyMGFiYSUyMGFiYSUyMGFiYSUyMGFiYSUyMGFiYSUyMGFiYSUyMGFiYSUyMGFiYSUyMGFiYSUyMGFiYSUyMGFiYSUyMGFiYQ==',
       },
       {
+        normal: false,
+        blur: 0.5,
+        wait: false,
         date: '3.5',
         msg: 'JUU5JTk4JUJGJUU1JUI3JUI0JUU5JTk4JUJGJUU1JUI3JUI0JUU5JTk4JUJGJUU1JUI3JUI0JUU5JTk4JUJGJUU1JUI3JUI0JUU5JTk4JUJGJUU1JUI3JUI0JUU5JTk4JUJGJUU1JUI3JUI0JUU5JTk4JUJGJUU1JUI3JUI0JUU5JTk4JUJGJUU1JUI3JUI0JUU5JTk4JUJGJUU1JUI3JUI0JUU5JTk4JUJGJUU1JUI3JUI0JUU5JTk4JUJGJUU1JUI3JUI0JUU5JTk4JUJGJUU1JUI3JUI0JUU5JTk4JUJGJUU1JUI3JUI0JUU5JTk4JUJGJUU1JUI3JUI0JUU5JTk4JUJGJUU1JUI3JUI0JUU5JTk4JUJGJUU1JUI3JUI0JUU5JTk4JUJGJUU1JUI3JUI0JUU5JTk4JUJGJUU1JUI3JUI0JUU5JTk4JUJGJUU1JUI3JUI0JUU5JTk4JUJGJUU1JUI3JUI0JUU5JTk4JUJGJUU1JUI3JUI0JUU5JTk4JUJGJUU1JUI3JUI0JUU5JTk4JUJGJUU1JUI3JUI0JUU5JTk4JUJGJUU1JUI3JUI0JUU5JTk4JUJGJUU1JUI3JUI0JUU5JTk4JUJGJUU1JUI3JUI0JUU5JTk4JUJGJUU1JUI3JUI0JUU5JTk4JUJGJUU1JUI3JUI0JUU5JTk4JUJGJUU1JUI3JUI0JUU5JTk4JUJGJUU1JUI3JUI0JUU5JTk4JUJGJUU1JUI3JUI0JUU5JTk4JUJGJUU1JUI3JUI0JUU5JTk4JUJGJUU1JUI3JUI0JUU5JTk4JUJGJUU1JUI3JUI0JUU5JTk4JUJGJUU1JUI3JUI0JUU5JTk4JUJGJUU1JUI3JUI0JUU5JTk4JUJGJUU1JUI3JUI0JUU5JTk4JUJGJUU1JUI3JUI0JUU5JTk4JUJGJUU1JUI3JUI0',
       },
     ],
   }),
   methods: {
-    encoded() {
-      if (!this.encryption) {
-        for (const i in this.msgList) {
-          this.msgList[i].msg = window.btoa(
-            encodeURIComponent(this.msgList[i].msg)
-          )
-        }
-        this.encryption = !this.encryption
+    encoded(id) {
+      if (this.msgList[id]) {
+        this.msgList[id].msg = window.btoa(
+          encodeURIComponent(this.msgList[id].msg)
+        )
       }
     },
-    decode() {
-      if (this.encryption) {
-        for (const i in this.msgList) {
-          this.msgList[i].msg = decodeURIComponent(
-            window.atob(this.msgList[i].msg)
-          )
-        }
-        this.encryption = !this.encryption
+    decode(id) {
+      if (this.msgList[id]) {
+        this.msgList[id].msg = decodeURIComponent(
+          window.atob(this.msgList[id].msg)
+        )
       }
     },
-    setNormal() {
-      if (this.normal) return
-      document.title = 'Diary'
-      this.blur = 10
-      setTimeout(() => {
-        this.decode()
-        this.blur = 0
-        this.normal = true
-        this.wait = false
-        localStorage.setItem('hrk', '?')
-      }, 500)
+    setNormal(id) {
+      if (this.msgList[id] && !this.msgList[id].normal) {
+        this.msgList[id].blur = 10
+        setTimeout(() => {
+          this.decode(id)
+          this.msgList[id].blur = 0
+          this.msgList[id].normal = true
+        }, 500)
+      }
     },
-    setAbnormal() {
-      if (!this.normal) return
-      localStorage.removeItem('hrk')
-      document.title = '□□□□□'
-      this.blur = 10
-      setTimeout(() => {
-        this.encoded()
-        this.blur = 0
-        this.normal = false
-        this.wait = false
-      }, 500)
+    setAbnormal(id) {
+      if (this.msgList[id] && this.msgList[id].normal) {
+        this.msgList[id].blur = 10
+        setTimeout(() => {
+          this.encoded(id)
+          this.msgList[id].blur = 0.5
+          this.msgList[id].normal = false
+        }, 500)
+      }
     },
-    change() {
-      if (this.wait) return
-      if (this.normal) {
-        axios.get('https://hrk-serve.vercel.app/api/abnormal?k=hrk')
+    change(id) {
+      if (this.msgList[id].wait) return
+      if (this.msgList[id].normal) {
+        axios.get(`http://hrk-serve.vercel.app/api/abnormal?k=hrk&id=${id}`)
+          .finally(() => {
+            this.msgList[id].wait = false
+          })
       } else {
-        axios.get('https://hrk-serve.vercel.app/api/normal?k=hrk')
+        axios.get(`http://hrk-serve.vercel.app/api/normal?k=hrk&id=${id}`)
+          .finally(() => {
+            this.msgList[id].wait = false
+          })
       }
-      this.wait = true
+      this.msgList[id].wait = true
     },
   },
-  mounted() {
-    this.normal = !!localStorage.getItem('hrk')
-    axios.get('https://hrk-serve.vercel.app/api/state').then((res) => {
-      if (res.data === 1) {
-        this.setNormal()
-      } else {
-        this.setAbnormal()
-      }
-    })
-    if (this.normal) {
-      this.decode()
-      document.title = 'Diary'
+  computed: {
+    normal() {
+      return this.msgList.every(item => item.normal)
     }
+  },
+  watch: {
+    normal(newVal) {
+      if (newVal) {
+        document.title = 'Diary'
+      } else {
+        document.title = '□□□□□'
+      }
+    }
+  },
+  mounted() {
+    axios.get('http://hrk-serve.vercel.app/api/state')
+      .then((res) => {
+        if (res.data) {
+          res.data.forEach(id => {
+            this.setNormal(id)
+          })
+        }
+      })
+      .finally(() => {
+        this.loading = false
+      })
 
     this.ready = true
   }
